49d160b3ae19c5d2ef9091954aa2c6808f8147c8
backfromsolaris <porraszach@gmail.com>
2021-10-25 17:55:47 -0500

    github nav link

--- marvel_api/api/routes.py
+++ marvel_api/api/routes.py
@@ -1,6 +1,6 @@
-from flask import Blueprint, json, request, jsonify
+from flask import Blueprint, request, jsonify
 from marvel_api.helpers import token_required
-from marvel_api.models import db, User, Hero, hero_schema, heroes_schema
+from marvel_api.models import db, Hero, hero_schema, heroes_schema
 
 
 api = Blueprint('api', __name__, url_prefix='/api')
